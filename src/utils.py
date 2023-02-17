d2833a68fd756ee7c79e12831c900bcc4bbf9f16
Fangrui Liu <fangruil@moqi.ai>
2023-02-17 18:36:07 +0800

    update configurable init prompt

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,14 @@
 ########################################################################################################
 
 import json, time, random, os
+import copy
 import numpy as np
 import torch
 from torch.nn import functional as F
 from tokenizers import Tokenizer
+import yaml
+from yaml import CSafeLoader
+from inspect import isfunction
 
 time_slot = {}
 time_ref = time.time_ns()
@@ -62,3 +66,60 @@ class TOKENIZER():
                 probs = probs.pow(1.0 / temperature)
             out = torch.multinomial(probs, num_samples=1)[0]
             return int(out)
+
+class toArgs():
+    def __init__(self, kwargs):
+        for k, v in kwargs.items():
+            self.__setattr__(k, v)
+
+    def save(self, f):
+        if type(f) is str:
+            f = open(f, 'w')
+        yaml.dump(self.get_dict(), f)
+
+    def load(self, f):
+        if type(f) is str:
+            f = open(f, 'r')
+        for k, v in yaml.load(f, CSafeLoader).items():
+            self.__setattr__(k, v)
+
+    def get_dict(self):
+        _d = {}
+        for k, v in self.__dict__.items():
+            if '__' not in k or not isfunction(v):
+                _d[k] = v
+        return _d
+
+    def __str__(self):
+        _d = self.get_dict()
+        max_len = max([len(k) for k in _d])
+        s = [f"{max_len * '='} CONFIGURATION {max_len * '='}"]
+        for k, v in _d.items():
+            s.append(k.ljust(max_len) + ': ' + str(v))
+        return '\n'.join(s)
+    
+def construct_prompt(f):
+    if type(f) is str:
+        f = open(f, 'r')
+    obj = yaml.load(f, CSafeLoader)
+    interface = obj['interface']
+    prompt = obj['init_prompt']
+    d = {}
+    for mode in ['qa', 'chat']:
+        d[mode] = {}
+        usr_suffix, bot_suffix, intro = 'user', 'bot', 'intro'
+        d[mode][usr_suffix] = f"{obj['_'.join([mode, usr_suffix])]}{interface}"
+        d[mode][bot_suffix] = f"{obj['_'.join([mode, bot_suffix])]}{interface}"
+        _intro = obj['_'.join([mode, intro])] 
+        _intro = _intro \
+            .replace('{bot}', d[mode][bot_suffix]) \
+            .replace('{user}', d[mode][usr_suffix])
+        _prompt = copy.deepcopy(prompt)
+        d[mode]['init_prompt'] = _prompt \
+            .replace('{bot}', d[mode][bot_suffix]) \
+            .replace('{user}', d[mode][usr_suffix]) \
+            .replace('{interface}', interface) \
+            .replace('{intro}', _intro) 
+        
+    return d
+    
