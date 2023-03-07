8c1d57cfd03ddbd0ee8d9c3e3e5d80ce5ef5b4ce
BlinkDL <a@a.com>
2023-03-07 06:00:57 +0000

    bug fix

--- rwkv_pip_package/src/rwkv/utils.py
+++ rwkv_pip_package/src/rwkv/utils.py
@@ -39,9 +39,9 @@ class PIPELINE():
     def decode(self, x):
         return self.tokenizer.decode(x)
 
-    def sample_logits(self, logits, temperature=1.0, top_p=1.0, top_k=50):
+    def sample_logits(self, logits, temperature=1.0, top_p=1.0, top_k=0):
         probs = F.softmax(logits.float(), dim=-1)
-
+        top_k = int(top_k)
         if probs.device == torch.device('cpu'):
             probs = probs.numpy()
             sorted_ids = np.argsort(probs)
@@ -49,8 +49,8 @@ class PIPELINE():
             cumulative_probs = np.cumsum(sorted_probs)
             cutoff = float(sorted_probs[np.argmax(cumulative_probs > top_p)])
             probs[probs < cutoff] = 0
-            if top_k < len(probs):
-                probs[sorted_ids[:-int(top_k)]] = 0
+            if top_k < len(probs) and top_k > 0:
+                probs[sorted_ids[:-top_k]] = 0
             if temperature != 1.0:
                 probs = probs ** (1.0 / temperature)
             probs = probs / np.sum(probs)
@@ -63,8 +63,8 @@ class PIPELINE():
             cumulative_probs = torch.cumsum(sorted_probs, dim=-1).cpu().numpy()
             cutoff = float(sorted_probs[np.argmax(cumulative_probs > top_p)])
             probs[probs < cutoff] = 0
-            if top_k < len(probs):
-                probs[sorted_ids][:-int(top_k)] = 0
+            if top_k < len(probs) and top_k > 0:
+                probs[sorted_ids[:-top_k]] = 0
             if temperature != 1.0:
                 probs = probs ** (1.0 / temperature)
             out = torch.multinomial(probs, num_samples=1)[0]
