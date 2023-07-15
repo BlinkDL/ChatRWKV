db7bdb42855c57204dd7633f34416658f779d13b
BlinkDL <a@a.com>
2023-07-15 09:03:31 +0000

    bugfix

--- music/run.py
+++ music/run.py
@@ -31,16 +31,16 @@ pipeline = PIPELINE(model, "tokenizer-midi.json")
 for TRIAL in range(10):
     print(TRIAL)
 
-    ccc = '<start>'
-    ccc_real = ccc
+    ccc = '<pad>' # the model is trained using <pad> (0) as separator
+    ccc_output = '<start>' # however the str_to_midi.py requires <start> & <end> as separator
 
     # uncomment this to continue a melody
     # ccc = "v:5b:3 v:5b:2 t125 t125 t125 t106 pi:43:5 t24 pi:4a:7 t15 pi:4f:7 t17 pi:56:7 t18 pi:54:7 t125 t49 pi:51:7 t117 pi:4d:7 t125 t125 t111 pi:37:7 t14 pi:3e:6 t15 pi:43:6 t12 pi:4a:7 t17 pi:48:7 t125 t60 pi:45:7 t121 pi:41:7 t125 t117 s:46:5 s:52:5 f:46:5 f:52:5 t121 s:45:5 s:46:0 s:51:5 s:52:0 f:45:5 f:46:0 f:51:5 f:52:0 t121 s:41:5 s:45:0 s:4d:5 s:51:0 f:41:5 f:45:0 f:4d:5 f:51:0 t102 pi:37:0 pi:3e:0 pi:41:0 pi:43:0 pi:45:0 pi:48:0 pi:4a:0 pi:4d:0 pi:4f:0 pi:51:0 pi:54:0 pi:56:0 t19 s:3e:5 s:41:0 s:4a:5 s:4d:0 f:3e:5 f:41:0 f:4a:5 f:4d:0 t121 v:3a:5 t121 v:39:7 t15 v:3a:0 t106 v:35:8 t10 v:39:0 t111 v:30:8 v:35:0 t125 t117 v:32:8 t10 v:30:0 t125 t125 t103 v:5b:0 v:5b:0 t9 pi:4a:7"
-    # ccc = '<start> ' + ccc
-    # ccc_real = '<start> pi:4a:7'
+    # ccc = '<pad> ' + ccc
+    # ccc_output = '<start> pi:4a:7'
 
     fout = open(f"midi_{TRIAL}.txt", "w")
-    fout.write(ccc_real)
+    fout.write(ccc_output)
 
     occurrence = {}
     state = None
